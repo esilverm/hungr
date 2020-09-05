28b88c6b8d30d661a4568a99a77664504aceeea1
Alice Wang <alice.wang428@gmail.com>
2020-09-04 20:25:57 -0400

    Styled card: added more info, implemented figma design

--- frontEnd/hungr/Card.js
+++ frontEnd/hungr/Card.js
@@ -1,15 +1,25 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, Image } from 'react-native';
+import { isRequired } from 'react-native/Libraries/DeprecatedPropTypes/DeprecatedColorPropType';
 
 const Card = () => {
   return(
     <View style={styles.Card}>
-      <Text h1> 
-        Chipotle 
-      </Text>
-      <Text style={styles.Text}>
-        134 E 8th Street
-      </Text>
+      <Image source = {require("./assets/chipotle.jpg")} style={styles.Image}/>
+      <View style={styles.Text}>
+        <View style={styles.Location}>
+          <Text style={styles.Name}> 
+            Chipotle ★★☆☆☆
+          </Text>
+          <Text>
+            134 E 8th Street {"\n"}
+            New York, NY 10003
+          </Text>
+        </View>
+        <Text style={styles.Price}>
+          $
+        </Text>
+      </View>
     </View>
   )
 }
@@ -17,13 +27,44 @@ const Card = () => {
 const styles = StyleSheet.create({
   Card: {
     width: 300,
-    height: 100,
+    height: 300,
     backgroundColor: "lightblue",
     display: "flex",
-    justifyContent: "center",
-    alignItems: "center"
+    borderRadius: 10,
+    overflow: "hidden",
+    shadowColor: "#000",
+    shadowOffset: {
+	    width: 0,
+	    height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 5,
   },
   Text: {
+    height: 100,
+    width: 300,
+    display: "flex",
+    flexDirection: "row",
+    // alignItems: "center",
+    justifyContent: "space-between",
+    paddingTop: 20
+  },
+  Image: {
+    width: 300,
+    height: "100%",
+    maxHeight: 200,
+    resizeMode: "contain"
+  },
+  Location: {
+    paddingLeft: 20
+  },
+  Name: {
+    fontSize: 18
+  },
+  Price: {
+    paddingRight: 20,
+    paddingTop: 2
   }
 })
 
