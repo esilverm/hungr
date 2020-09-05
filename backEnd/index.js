ac83f6a339934b4c62bc957d5d89ce945ae039fb
Bea Litonjua <bealitonjua@beas-air.home>
2020-09-04 20:34:08 -0400

    backend stuff

--- backEnd/index.js
+++ backEnd/index.js
@@ -1,33 +1,9 @@
 
-// const jwt = require('jsonwebtoken')
-const bcrypt = require('bcrypt')
 const express = require('express')
 const http = require('http')
 const app = express()
 const server = http.createServer(app)
-
-app.post('/', async (request, response) => {
-    const username = request.username;
-    const password = request.password;
-
-    if (!username || !password) {
-        return response.status(404).json("Invalid credentials")
-    }
-
-    /* Database logic here for username here */
-
-    const decryptP = await bcrypt.compare(password, db.password)
-    
-    if (!decryptP) {
-        return response.status(404).json("Invalid password")
-    } 
-
-    /* jwt logic here */
-
-    return response.status(200).json("Login successful")
-
-
-})
+const db = require('./db')
 
 app.get('/', (request, response) => {
     return response.json("Hello")
