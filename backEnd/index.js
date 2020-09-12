7b6ba316d8845d9c390d4db80c17d40f43c3125f
Anthony Ardi Sukotjo <sukotjo33@yahoo.com.sg>
2020-09-12 08:32:50 +0800

    Adding get end point for restaurant

--- backEnd/index.js
+++ backEnd/index.js
@@ -11,3 +11,30 @@ app.get("/", (request, response) => {
 server.listen(3001, () => {
   console.log("Server running on port 3001");
 });
+
+app.get("/restaurants",(request, response) => {
+  const deviceId = request.query.deviceId;
+  const sql = `SELECT * FROM restaurants 
+      inner join 
+      (select devices.deviceid,
+      relationships.restaurantid,
+       relationships.isLiked 
+        from devices 
+        inner join relationships 
+        on relationships.device = devices.id 
+        where devices.deviceid = (?)) 
+        as output 
+        on restaurants.id = output.restaurantid;`
+  console.log(deviceId);
+  db.all(sql,[deviceId],(err,row)=>{
+    if(err){
+      console.log(err);
+    } else {
+      console.log(row);
+      return response.json(row);
+    }
+  })
+  //return response.json({message: deviceId});
+});
+
+
