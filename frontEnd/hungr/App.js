678548b6b824920dfd4fc60534da4113e098d721
Keerthana <63519382+keerthana-github@users.noreply.github.com>
2020-09-14 20:22:07 -0400

    location async

--- frontEnd/hungr/App.js
+++ frontEnd/hungr/App.js
@@ -1,10 +1,25 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { useEffect } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import Card from './Card';
 import RestaurantView from './RestaurantView';
+import * as Location from 'expo-location';
 
 export default function App() {
+  const [location, setLocation] = useState(null);
+  const [errorMsg, setErrorMsg] = useState(null);
+  useEffect (() => {
+    (async() => {
+      let {status}= await Location.requestPermissionsAsync();      
+      if (status !== 'granted') {
+        setErrorMsg('Permission to access location was denied');
+      }
+      let location = await Location.getCurrentPositionAsync({});
+      setLocation(location);
+    })
+  })
+  
+  
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
