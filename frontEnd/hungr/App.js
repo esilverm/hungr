95139d7f49d77344ba54e2b7aea7f8ddbc2c1500
Nicholas Yang <ny585@nyu.edu>
2020-09-19 14:52:42 -0400

    Started request logic

--- frontEnd/hungr/App.js
+++ frontEnd/hungr/App.js
@@ -1,5 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import Card from './Card';
 import RestaurantView from './RestaurantView';
@@ -8,26 +8,39 @@ import * as Location from 'expo-location';
 export default function App() {
   const [location, setLocation] = useState(null);
   const [errorMsg, setErrorMsg] = useState(null);
-  useEffect (() => {
+  useEffect(() => {
     (async() => {
-      let {status}= await Location.requestPermissionsAsync();      
+      let {status}= await Location.requestPermissionsAsync();
       if (status !== 'granted') {
         setErrorMsg('Permission to access location was denied');
       }
       let location = await Location.getCurrentPositionAsync({});
       setLocation(location);
-    })
-  })
-  
-  
+    })();
+    getRestaurantData();
+  }, [])
+
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>{JSON.stringify(location)}</Text>
       {/*<Card/>*/}
       <RestaurantView />
       <StatusBar style="auto" />
     </View>
   );
+}
+
+async function getRestaurantData(location) {
+  const searchParams = new URLSearchParams();
+  searchParams.append('input', 'restaurant');
+  searchParams.append('inputtype', 'textquery');
+  // searchParams.append('locationbias', 'circle:2000@47.6918452,-122.2226413');
+
+  const res = await fetch('https://maps.googleapis.com/maps/api/place/findplacefromtext/json?' + searchParams.toString());
+
+
+  console.log(res)
 }
 
 const styles = StyleSheet.create({
