b287bb2f25d691f31fbe1f2f6fba19a10a58d750
Khanh Nguyen <khanhnguyen99hn@gmail.com>
2020-09-20 13:30:25 -0400

    finishing backend with 2 endpoints for get and post

--- backEnd/db.js
+++ backEnd/db.js
@@ -1,37 +1,29 @@
-
-
 const sqlite3 = require("sqlite3").verbose();
 let db = new sqlite3.Database("./hungr.db", (err) => {
   if (err) {
     console.log(err);
   } else {
     console.log("Connected to database.");
-    db.run(`CREATE TABLE 
-    IF NOT EXISTS devices 
-    (device_id text primary key)`);
-
     db.run(
-      `CREATE TABLE 
-      IF NOT EXISTS restaurants 
-       (id integer 
-        primary key autoincrement, 
+      `create table if not exists devices (
+        device_id text primary key)`
+    );
+    db.run(
+      `create table if not exists restaurants (
+        id integer primary key autoincrement, 
         name text, 
         address text, 
         city text, 
         state text, 
         zipcode text)`
-
     );
     db.run(
-      `CREATE TABLE 
-      IF NOT EXISTS relationships 
-      (id integer primary key autoincrement, 
-        deviceid integer, 
-        restaurantid integer, 
-        leftOrRight text, 
-        foreign key (deviceid) references devices (id), 
-        foreign key (restaurantid) references restaurants (id), 
-        check(leftOrRight = "left" OR leftOrRight = "right"))`
+      `create table if not exists swipes (
+        device_id integer, 
+        restaurant_id integer,
+        foreign key (device_id) references devices (device_id), 
+        foreign key (restaurant_id) references restaurants (id),
+        primary key (device_id, restaurant_id))`
     );
   }
 });
--- backEnd/index.js
+++ backEnd/index.js
@@ -1,44 +1,139 @@
 const express = require("express");
 const http = require("http");
+const db = require("./db");
+const cors = require("cors");
+
+const PORT = process.env.PORT || 3001;
+
 const app = express();
 const server = http.createServer(app);
-const db = require("./db");
-
-app.get("/", (request, response) => {
-  return response.json("Hello");
-});
-
-server.listen(3001, () => {
-  console.log("Server running on port 3001");
-});
-
+app.use(express.json());
+app.use(cors());
 
 app.get("/restaurants", (request, response) => {
-  const deviceId = request.query.deviceId;
-  const sql = `SELECT
-  restaurants.name,
-  restaurants.address,
-  restaurants.city,
-  restaurants.state,
-  restaurants.zipcode
-  FROM restaurants INNER JOIN 
-  (SELECT devices.device_id, 
-    relationships.restaurantid, 
-    relationships.leftOrRight 
-    FROM devices 
-    INNER JOIN relationships
-    ON relationships.deviceid=devices.device_id
-    WHERE devices.device_id=(?)) 
-    AS output
-    ON restaurants.id=output.restaurantid;`;
-
-  console.log(deviceId);
-  db.all(sql, [deviceId], (err, row) => {
+  const deviceID = request.query.device;
+  const sql = `SELECT 
+  * from SWIPES INNER JOIN 
+  restaurants ON 
+  swipes.restaurant_id=restaurants.id 
+  WHERE swipes.device_id=(?);`;
+  db.all(sql, [deviceID], (err, rows) => {
     if (err) {
       console.log(err);
     } else {
-      console.log(row);
-      return response.json(row);
+      const restaurants = [];
+      rows.forEach((row) => {
+        restaurants.push({
+          name: row.name,
+          address: row.address,
+          city: row.city,
+          state: row.state,
+          zipcode: row.zipcode,
+        });
+      });
+      return response.status(200).json(restaurants);
     }
   });
+});
+
+// {
+//   "device_id": 209858979,
+//   "name": "Pizza Hut",
+//   "address": "3000 Broadway",
+//   "city": "New York",
+//   "state": "NY",
+//   "zipcode": "10001"
+// }
+app.post("/swiped", (req, res) => {
+  const { deviceID, name, address, city, state, zipcode } = req.body;
+  const selectRes = `SELECT id FROM RESTAURANTS 
+    WHERE name=(?) AND address=(?) AND city=(?)
+    AND state=(?) AND zipcode=(?)`;
+  const insertDevice = `INSERT INTO devices(device_id) VALUES(?)`;
+  const insertSwipe = `INSERT INTO 
+    swipes(device_id, restaurant_id) 
+    values(?,?)`;
+  const insertRes = `INSERT 
+        INTO restaurants(name, address, 
+        city, state, zipcode) values(?,?,?,?,?)`;
+
+  //insert device into the table
+  db.run(insertDevice, [deviceID], (err) => {
+    if (err) {
+      console.log(`Device ${deviceID} already exists!`);
+    }
+  });
+
+  //select restaurant to check duplicate
+  const selectRetaurant = new Promise((resolve, reject) => {
+    db.get(selectRes, [name, address, city, state, zipcode], (err, row) => {
+      if (err) {
+        console.log(
+          `${name}, ${address},${city}, ${state}, ${zipcode} already exists!`
+        );
+        reject(err);
+      }
+      resolve(row);
+    });
+  });
+
+  const insertNewSwipe = (deviceID, restaurantID) => {
+    db.run(insertSwipe, [deviceID, restaurantID], (err) => {
+      if (err) {
+        return res.status(409).json({ message: "Swipe already exists!" });
+      }
+      return res.status(201).json({ message: "Successful" });
+    });
+  };
+
+  selectRetaurant
+    .then((restaurantID) => {
+      if (restaurantID) {
+        const id = restaurantID.id;
+        //restaurant already exists
+        insertNewSwipe(deviceID, id);
+      } else {
+        //insert restaurant and grab the index
+        const insertRestaurant = new Promise((resolve, reject) => {
+          db.run(insertRes, [name, address, city, state, zipcode], function (
+            err
+          ) {
+            if (err) {
+              reject(err);
+            }
+            resolve(this.lastID);
+          });
+        });
+        insertRestaurant
+          .then((newRestaurantID) => {
+            insertNewSwipe(deviceID, newRestaurantID);
+          })
+          .catch((err) => {
+            console.log(err);
+          });
+      }
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
+app.post("/device", (req, res) => {
+  try {
+    const { deviceID } = req.body;
+    const newRelationship = db.run(
+      `INSERT INTO devices (device_id) VALUES (${deviceID})`
+    );
+    res.json("device: " + deviceID);
+  } catch (err) {
+    console.error(err);
+  }
+});
+
+app.get("/", (request, response) => {
+  return response.status(200).json({ message: "Welcome to hungr API!" });
+});
+
+server.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
